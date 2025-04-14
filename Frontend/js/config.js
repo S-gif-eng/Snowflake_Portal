a3660ceba43eb69c06b92402dd36a05eab67a5f4
Shaik Inthiyaz <skinthiyaz9581@gmail.com>
2025-04-14 17:31:52 +0530

    config urlchanges

--- Frontend/js/config.js
+++ Frontend/js/config.js
@@ -1,3 +1,4 @@
 const CONFIG = {
-    API_BASE_URL: "http://127.0.0.1:5000/api/snowflake", // Change this to your backend base URL
+    //API_BASE_URL: "http://127.0.0.1:5000/api/snowflake", // Change this to your backend base URL
+    API_BASE_URL: "https://snowflake-portal.onrender.com/api/snowflake"
 };
